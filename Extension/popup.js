d56bacbd51f41353760b947ba8cf5883a95ecb54
Balagajaraj Prabhakar <93154220+BalagajarajPrabhakar@users.noreply.github.com>
2024-12-02 11:00:13 +0530

    Update popup.js

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,9 +1,7 @@
 document.getElementById("get-text").addEventListener("click", () => {
-  // Query the active tab
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     if (!tabs || !tabs.length) return;
 
-    // Send a message to the content script in the active tab
     chrome.tabs.sendMessage(tabs[0].id, { action: "getSelectedText" }, (response) => {
       if (chrome.runtime.lastError) {
         console.error(chrome.runtime.lastError.message);
