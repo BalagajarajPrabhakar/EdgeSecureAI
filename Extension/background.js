9e4c80aa50d4a4345534ddbb4f538ca49033aae2
Balagajaraj Prabhakar <93154220+BalagajarajPrabhakar@users.noreply.github.com>
2024-12-02 10:57:46 +0530

    Update background.js

--- Extension/background.js
+++ Extension/background.js
@@ -1,15 +1,14 @@
 chrome.runtime.onInstalled.addListener(() => {
-  // Create the context menu option
   chrome.contextMenus.create({
     id: "getSelectedText",
     title: "Get Selected Text",
-    contexts: ["selection"], // Show only when text is selected
+    contexts: ["selection"], 
   });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "getSelectedText") {
-    // Inject content script to the active tab
+    
     chrome.scripting.executeScript(
       {
         target: { tabId: tab.id },
